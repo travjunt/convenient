29a6e63d5c0a3b56e1cc2ab4453173f9656039ce
Travis McCormick <tmccorm.dev@gmail.com>
2018-08-14 14:50:52 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Travis McCormick',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h2>iOS Developoer</h2>
+    <p>[Insert catchy bio here]</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
