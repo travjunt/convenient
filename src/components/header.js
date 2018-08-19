22af21091b2b0e68008b97579932cc8e88b77864
travjunt <travjunt@gmail.com>
2018-08-18 19:20:43 -0600

    Implemented Card component and Card.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-swift.png')} width="30" /></Link>
+      <Link to="/about">About</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/downloads"><button>Downloads</button></Link>
     </div>
   </div>
 )
