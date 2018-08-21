222e47f0848d6f726f0980a3a652883da48ccad4
Travis McCormick <travis@TravisMacBookPro.local>
2018-08-21 09:41:39 -0600

    css styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,41 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../images/logo-swift.png')} width="30" /></Link>
-      <Link to="/about">About</Link>
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/downloads"><button>Downloads</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true})
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-swift.png')} width="30" /></Link>
+          <Link to="/about">About</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/connect"><button>Github</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
