e2d169572f7580512992c69682ce81d840a2056a
travjunt <travjunt@gmail.com>
2018-08-22 14:59:44 -0600

    Added new section and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Card from '../components/Card';
+import Section from '../components/Section';
+import Wave from '../components/Wave';
 
 const IndexPage = () => (
   <div>
@@ -17,42 +19,40 @@ const IndexPage = () => (
           <img src={require('../images/logo-framer.png')} width="50" />
           <img src={require('../images/logo-studio.png')} width="50" />
         </div>
-          <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill="white">
-              <animate repeatCount="indefinite" fill="freeze" attributionName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-
-              M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
-          
-              M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-
-              M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
-              " />
-          </path>
-        </svg>
+        
       </div>
     </div>
-
     <div className="Cards">
       <h2>Portfolio</h2>
       <div className="CardGroup">
         <Card 
           title="Pitch Perfect"
           text="12 sections"
-          image={require('../images/jshine.jpg')} />
+          image={require('../images/jshine.jpg')} 
+          />
         <Card 
           title="MemeMe"
           text="12 sections"
-          image={require('../images/flare.jpg')} />
+          image={require('../images/flare.jpg')} 
+          />
         <Card 
           title="On The Map"
           text="12 sections"
-          image={require('../images/bydesign.jpg')} />
+          image={require('../images/bydesign.jpg')} 
+          />
         <Card 
           title="Virtual Tourist"
           text="12 sections"
-          image={require('../images/amin.jpg')} />
-        </div>
+          image={require('../images/amin.jpg')} 
+          />
+      </div>
     </div>
+    <Section 
+      image={require('../images/wallpaper2.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="Learning React"
+      text="Fill in a bunch of text here not sure yet."
+    />
   </div>
 )
 
