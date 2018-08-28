1f5e6b51d30310ed00fec146ea07dc203c9c0d33
travjunt <travjunt@gmail.com>
2018-08-28 12:22:59 -0600

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Travis McCormick',
     description: 'A portfolio website built from scratch by Travis McCormick.',
-    keyword: 'ios developer, ios portfolio, ios, swift developer, programmer, react developer, utah developer, salt lake city ios developer'
+    keywords: 'ios developer, ios portfolio, ios, swift developer, programmer, react developer, utah developer, salt lake city ios developer'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
